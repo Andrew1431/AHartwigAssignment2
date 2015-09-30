631de3b3b5b0b85db4acaa560771c05e32e4c6ab
Andrew Hartwig <andrew.hartwig1@gmail.com>
2015-09-30 10:48:49 -0400

    Set up game manager class and pawn class

--- AHartwigAssignment2/Form1.cs
+++ AHartwigAssignment2/Form1.cs
@@ -12,6 +12,7 @@ namespace AHartwigAssignment2
 {
     public partial class Form1 : Form
     {
+        
         public Form1()
         {
             InitializeComponent();
---
+++ AHartwigAssignment2/GameManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AHartwigAssignment2
+{
+    class GameManager
+    {
+
+
+        public GameManager()
+        {
+
+        }
+
+        class Pawn
+        {
+            private int X_POS;
+            private int Y_POS;
+            private string type;
+
+            public Pawn(string type, int xPosition, int yPosition)
+            {
+                X_POS = xPosition;
+                Y_POS = yPosition;
+            }
+
+            public int GetXPosition()
+            {
+                return X_POS;
+            }
+
+            public int GetYPosition()
+            {
+                return Y_POS;
+            }
+        }
+    }
+}
